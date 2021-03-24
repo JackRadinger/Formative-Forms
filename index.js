f831fee727bfb5357ed1d4c56fdfb8f1e58c7a8f
kbetker <kevin@betker.org>
2021-03-24 15:44:28 -0500

    on to phase 3

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require("express");
+const csrf = require("csurf");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
 app.use(express.urlencoded());
+app.use(cookieParser());
+const csrfProtection = csrf({ cookie: true });
 
 app.get("/", (req, res) => {
-  res.render('index', { title: 'Users', users } )
+  res.render('index', { users } )
 });
+
+app.get("/create", csrfProtection, (req, res) => {
+  res.render('create-form', { csrfToken: req.csrfToken()  })
+});
+
+app.post("/create", (req, res) => {
+  const user = {
+    // csrfToken: req.csrfToken(),
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  users.push(user);
+  res.redirect("/");
+
+})
 
 const users = [
   {
